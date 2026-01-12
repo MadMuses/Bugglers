74ded9098c9c13baacfabe654a643f6c3a2c6df0
MadMuses <mamusique128@gmail.com>
2026-01-12 17:54:43 +0100

    CHANGE : Color calc is now in a function

--- src/main.rs
+++ src/main.rs
@@ -36,22 +36,10 @@ fn main() {
         let amplitudes: Vec<f32> = signal.iter().map(|c| c.norm()).collect();
 
         // Find the 3 frequencies peaks
-        let mut max: Vec<(i32, f32)> =
-            vec![(0, amplitudes[0]), (0, amplitudes[0]), (0, amplitudes[0])];
-        for (freq, amp) in amplitudes.iter().take(num_samples / 2).enumerate() {
-            if *amp > max[0].1 {
-                max[2] = max[1];
-                max[1] = max[0];
-                max[0] = (freq as i32, *amp);
-            } else if *amp > max[1].1 {
-                max[2] = max[1];
-                max[1] = (freq as i32, *amp);
-            } else if *amp > max[2].1 {
-                max[2] = (freq as i32, *amp);
-            }
-        }
+        let colors: Vec<(i32, f32)> = get_colors_from_max(amplitudes,num_samples);
+
         *image.get_pixel_mut(pos % HEIGHT, pos / HEIGHT) = image::Rgb(
-            max.iter()
+            colors.iter()
                 .map(|(f, _)| (f * 255 / (num_samples as i32 / 2)) as u8)
                 .collect::<Vec<u8>>()
                 .try_into()
@@ -60,4 +48,23 @@ fn main() {
     }
 
     image.save("output/output.png").unwrap();
+}
+
+
+fn get_colors_from_max(amplitudes: Vec<f32>, num_samples: usize) -> Vec<(i32, f32)>
+{
+    let mut max = vec![(0, amplitudes[0]), (0, amplitudes[0]), (0, amplitudes[0])];
+    for (freq, amp) in amplitudes.iter().take(num_samples / 2).enumerate() {
+        if *amp > max[0].1 {
+            max[2] = max[1];
+            max[1] = max[0];
+            max[0] = (freq as i32, *amp);
+        } else if *amp > max[1].1 {
+            max[2] = max[1];
+            max[1] = (freq as i32, *amp);
+        } else if *amp > max[2].1 {
+            max[2] = (freq as i32, *amp);
+        }
+    }
+    return max;
 }
