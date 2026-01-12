9ee143289c4df79896136fa0c399277fc8043c15
MadMuses <mamusique128@gmail.com>
2026-01-12 17:17:30 +0100

    ADD : Now plots an image from the frequencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use hound;
 use num::complex::Complex;
 use rustfft::FftPlanner;
+use plotters::prelude::*;
 
 fn main() {
     let mut reader = hound::WavReader::open("samples/darude.wav").unwrap();
@@ -48,5 +49,36 @@ fn main() {
             max[2].0 * 255 / (num_samples as i32 / 2),
         ])
     }
-    println!("{:?}", pixels[18560]);
+    println!("{:?}", pixels.len());
+    println!("{:?}", 480*480);
+
+    save_image(&pixels, "output/image.png").unwrap();
+}
+
+
+fn save_image(pixels: &Vec<Vec<i32>>, output_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let root_area = BitMapBackend::new(output_path, (1024, 768)).into_drawing_area();
+    root_area.fill(&WHITE)?;
+    //let max_value = pixels.iter().flatten().cloned().fold(f32::MIN, i32::max);
+    //let min_value = pixels.iter().flatten().cloned().fold(f32::MAX, i32::min);
+
+    // Creating the chart canvas
+    let mut chart = ChartBuilder::on(&root_area)
+        //.caption("Image", ("sans-serif", 50))
+        .build_cartesian_2d(0..480,0..480)?;
+
+    // Draw
+    chart.configure_mesh().disable_mesh().draw()?;
+    for (pos, colors) in pixels.iter().enumerate() {
+        let color = RGBColor(colors[0] as u8,colors[1] as u8,colors[2] as u8);
+        
+        chart.draw_series(PointSeries::of_element(
+            vec![((pos % 480) as i32, (pos / 480) as i32)],
+            1,
+            &color,
+            &|c, s, st| { EmptyElement::at(c) + Circle::new((0,0), s, st.filled()) }
+        ))?;
+    }
+    root_area.present()?;
+    Ok(())
 }
