1fde0aa11369c5bcd86e891213f0bae0c5168c02
guewenhtbs <kerguegue@gmail.com>
2026-01-12 14:57:45 +0100

    (main): read wav file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use hound;
+
 fn main() {
-    println!("Hello, world!");
+    let mut reader = hound::WavReader::open("samples/miaou.wav").unwrap();
+
+    let spec = reader.spec();
+    println!("{:?}", spec);
+
+    let sqr_sum = reader.samples::<i16>().fold(0.0, |sqr_sum, s| {
+        let sample = s.unwrap() as f64;
+        // println!("{}", sample);
+        sqr_sum + sample * sample
+    });
+    println!("RMS is {}", (sqr_sum / reader.len() as f64).sqrt());
 }
